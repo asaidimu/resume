e791af39aa6940edc600f0c65b4d860f2e85d9b0
Augustine Saidimu <47994458+augustinesaidimu@users.noreply.github.com>
2020-11-15 15:20:50 +0300

    Refactored the JavaScript.
    Why:
        To improve code design and most importantly, to improve readability.
    
    How:
        By extracting functions and creating a single object.
        Modularisation.
    
    Tags:
        v0.23
    
    Changes to be committed:
           modified:   index.html
           modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,47 +1,59 @@
 (function (window) {
     "use strict";
-    window.addEventListener("load", (event) => {
-        let list_items = document.getElementsByClassName("nav-list-item");
+    let App = window.App || {};
 
-        Array.from(list_items).forEach((item) => {
-            const link = item.getElementsByTagName("a")[0];
+    class ScrollSpy {
+        constructor(indicators, targets) {
+            this.indicators = indicators;
+            this.targets = targets;
+            this.initIndicators();
+            this.offsets = this.getOffsets();
+        }
 
-            link.addEventListener("click", (e) => {
-                e.preventDefault();
-            });
+        initIndicators() {
+            this.indicators.forEach((indicator) => {
+                const link = indicator.getElementsByTagName("a")[0];
 
-            item.addEventListener("click", (event) => {
-                event.preventDefault();
-                const href = link.getAttribute("href");
-                document.querySelector(href).scrollIntoView({
-                    behavior: "smooth",
+                link.addEventListener("click", (e) => {
+                    e.preventDefault();
+                });
+
+                indicator.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    const href = link.getAttribute("href");
+                    document.querySelector(href).scrollIntoView({
+                        behavior: "smooth",
+                    });
                 });
             });
-        });
+        }
 
-        let sections = document.getElementsByTagName("section");
-        let offsets = {};
-        Array.from(sections).forEach((section) => {
-            offsets[section.getAttribute("id")] = section.offsetTop;
-        });
+        getOffsets() {
+            let offsets = {};
+            this.targets.forEach((target) => {
+                offsets[target.getAttribute("id")] = target.offsetTop;
+            });
 
-        let links = Array.from(list_items);
+            return offsets;
+        }
 
-        window.onscroll = () => {
+        onScroll() {
             var scrollPosition =
                 document.documentElement.scrollTop || document.body.scrollTop;
             scrollPosition += 64;
-            for (let id in offsets) {
-                if (offsets[id] < scrollPosition) {
-                    console.log(offsets[id]);
-                    links.forEach((link) => {
-                        link.classList.remove("active");
+
+            for (let id in this.offsets) {
+                if (this.offsets[id] < scrollPosition) {
+                    this.indicators.forEach((indicator) => {
+                        indicator.classList.remove("active");
                     });
                     document
                         .querySelector("[data-link-section='" + id + "']")
                         .classList.add("active");
                 }
             }
-        };
-    });
+        }
+    }
+    App.ScrollSpy = ScrollSpy;
+    window.App = App;
 })(window);
